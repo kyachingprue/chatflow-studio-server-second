8a8ec1963634f11e5c81449bceda8aec767ad88c
kyachingprue <kyachingpruemarma.info@gmail.com>
2025-12-30 23:00:55 +0600

    new url updated

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ const server = http.createServer(app);
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'https://chatflow-chat-studio.vercel.app',
+    ],
     credentials: true,
   })
 );
